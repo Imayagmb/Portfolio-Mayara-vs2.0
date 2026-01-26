5aa4b1eefffd188645be3e9147f83dca51ce0ae8
mayaramonteiro <imayagmb@gmail.com>
2026-01-26 14:52:38 -0300

    fix: Corrigindo email, localização, alterando icones na página footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Github, Linkedin, Coffee, Heart } from 'lucide-react';
+import { Github, Linkedin} from 'lucide-react';
+import { FaReact } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
+
 
 interface FooterProps {
   isDarkMode: boolean;
@@ -14,9 +19,9 @@ const Footer: React.FC<FooterProps> = ({ isDarkMode }) => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Brand */}
           <div className="text-center md:text-left">
-            <h3 className="text-xl font-bold mb-4">Mayara Gonçalves Monteiro</h3>
+            <h3 className="text-xl font-bold mb-4">Mayara  Monteiro</h3>
             <p className="text-gray-400 mb-4">
-              Desenvolvedora Full Stack apaixonada por tecnologia e inovação.
+              Desenvolvedora Full Stack transformando ideias em código.
             </p>
             <div className="flex justify-center md:justify-start space-x-4">
               <a
@@ -90,7 +95,8 @@ const Footer: React.FC<FooterProps> = ({ isDarkMode }) => {
           {/* contato Info */}
           <div className="text-center md:text-right">
             <h4 className="text-lg font-semibold mb-4">Contato</h4>
-            <p className="text-gray-400 mb-2">
+             <p className="text-gray-400 mb-2 flex items-center justify-center md:justify-end gap-2">
+    <MdEmail size={18} />
               <a 
                 href="mailto:imayagmb@gmail.com"
                 className="hover:text-white transition-colors"
@@ -98,7 +104,8 @@ const Footer: React.FC<FooterProps> = ({ isDarkMode }) => {
                 imayagmb@gmail.com
               </a>
             </p>
-            <p className="text-gray-400">
+            <p className="text-gray-400 flex items-center justify-center md:justify-end gap-2">
+    <FaLocationDot size={16} />
               Rio de Janeiro, RJ
             </p>
           </div>
@@ -108,15 +115,13 @@ const Footer: React.FC<FooterProps> = ({ isDarkMode }) => {
         <div className="mt-8 pt-8 border-t border-gray-800">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm mb-4 md:mb-0">
-              © {currentYear} Mayara Gonçalves Monteiro. Todos os direitos reservados.
+              © {currentYear} Mayara Monteiro. Todos os direitos reservados.
             </p>
             <div className="flex items-center text-gray-400 text-sm">
               <span className="flex items-center">
                 Desenvolvido com
-                <Heart size={16} className="mx-1 text-red-500" />
-                React.js, e muito
-                <Coffee size={16} className="mx-1 text-[#0F96AE]" />
-                café
+                <FaReact size={16} className="mx-1 text-cyan-400" />
+                React.js                
               </span>
             </div>
           </div>
